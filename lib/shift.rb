2a85737f05524d39a2bb0e48a082ea79de064b59
Shendidy <sherif@shendidy.com>
2019-05-05 03:54:20 +0100

    implemented shift method

--- lib/shift.rb
+++ lib/shift.rb
@@ -1,5 +1,21 @@
 class Shift
+  def initialize
+    @counter = 0
+  end
+
   def shift_left(s1, s2)
-    0
+    return @counter if s1 == s2
+    check_texts(s1, s2)
+  end
+
+  private
+
+  def check_texts(s1, s2)
+    s1.length >= s2.length ? 
+      s1 = s1.split("").drop(1).join("") : 
+      s2 = s2.split("").drop(1).join("")
+
+    @counter += 1
+    shift_left(s1, s2)
   end
 end
--- spec/shift_spec.rb
+++ spec/shift_spec.rb
@@ -4,4 +4,8 @@ describe Shift do
   it '1- should return 0' do
     expect(subject.shift_left("test1", "test1")).to eq(0)
   end
+
+  it '1- should return 2' do
+    expect(subject.shift_left("test", "west")).to eq(2)
+  end
 end
