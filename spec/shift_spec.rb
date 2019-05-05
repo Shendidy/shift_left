a15bc1f1cc085b2353c3284a977248a6e84f27eb
Shendidy <sherif@shendidy.com>
2019-05-05 03:56:39 +0100

    completed application as per specifications in codewars.com

--- spec/shift_spec.rb
+++ spec/shift_spec.rb
@@ -5,7 +5,15 @@ describe Shift do
     expect(subject.shift_left("test1", "test1")).to eq(0)
   end
 
-  it '1- should return 2' do
+  it '2- should return 2' do
     expect(subject.shift_left("test", "west")).to eq(2)
+  end
+
+  it '3- should return 7' do
+    expect(subject.shift_left("test", "yes")).to eq(7)
+  end
+
+  it '4- should return 1' do
+    expect(subject.shift_left("b", "ab")).to eq(1)
   end
 end
