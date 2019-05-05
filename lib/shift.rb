5b059cb8c16680a21f66924353d5f8ba1071051e
Shendidy <sherif@shendidy.com>
2019-05-05 04:46:13 +0100

    refactored my code

--- lib/shift.rb
+++ lib/shift.rb
@@ -1,21 +1,16 @@
 class Shift
-  def initialize
-    @counter = 0
-  end
 
   def shift_left(s1, s2)
-    return @counter if s1 == s2
-    check_texts(s1, s2)
+    counter = 0
+    
+    while s1 != s2
+      s1.length >= s2.length ? 
+        s1 = s1.split("").drop(1).join("") : 
+        s2 = s2.split("").drop(1).join("")
+        counter += 1
+    end
+
+    counter
   end
 
-  private
-
-  def check_texts(s1, s2)
-    s1.length >= s2.length ? 
-      s1 = s1.split("").drop(1).join("") : 
-      s2 = s2.split("").drop(1).join("")
-
-    @counter += 1
-    shift_left(s1, s2)
-  end
 end
